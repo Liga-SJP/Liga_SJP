2aae3c1e44799a1402e4380864050468557c1339
rafael1392barca <frafaelsilvero@gmail.com>
2026-09-24 00:21:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE = 'liga-crm-v1';
+const CACHE = 'liga-crm-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-self.addEventListener('activate', e=>{ self.clients.claim(); });
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request).then(cached=>
-      cached || fetch(e.request).then(res=>{
-        if(e.request.method==='GET' && res.ok){
-          const clone = res.clone();
-          caches.open(CACHE).then(c=>c.put(e.request, clone));
-        }
-        return res;
-      }).catch(()=>cached)
-    )
+    fetch(e.request).then(res=>{
+      if(e.request.method==='GET' && res.ok){
+        const clone = res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, clone));
+      }
+      return res;
+    }).catch(()=> caches.match(e.request))
   );
 });
